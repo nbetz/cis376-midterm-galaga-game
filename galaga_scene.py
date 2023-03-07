88b53e69017443947f290c4cdcdac2a5cca56e61
Lren1300 <reneaul@mail.gvsu.edu>
2023-03-07 14:01:53 -0500

    Trying to blit from sprite sheet

--- galaga_scene.py
+++ galaga_scene.py
@@ -1,4 +1,5 @@
 
+
 import pygame
 import scene
 import game_object
@@ -7,20 +8,21 @@ import engine
 
 class GalagaScene(scene.Scene):
     def __init__(self):
-        super().__init__(self)
+        super().__init__()
+        self.sprite_set = game_object.SpriteSet(self)
         enemies = pygame.sprite.Group()
         player = pygame.sprite.Group()
         all_sprites = pygame.sprite.Group()
         self.groups.update({"enemies": enemies, "player": player, "all_sprites": all_sprites})
 
-
-    def initial_scene(self):
-        pass
-
     def draw(self):
-        pass
+        self.sprite_set.pieces[0].blitme()
+        pygame.display.flip()
 
     def check_win(self) -> bool:
         return False
+
+
+
 
 
--- game_object.py
+++ game_object.py
@@ -6,10 +6,9 @@ Provides basic functions for creating GameObjects for a game.
 import pygame
 import random
 
+from sprite_sheet import SpriteSheet
 from pygame.sprite import AbstractGroup
-
 import engine
-import scene
 
 
 class GameObject(pygame.sprite.DirtySprite):
@@ -34,3 +33,52 @@ class GameObject(pygame.sprite.DirtySprite):
         self.last_x = x
         self.last_y = y
         self.scene = in_scene
+
+
+class SpriteSet:
+    def __init__(self, in_scene: "Scene"):
+        """Initialize attributes to represent the overall set of pieces."""
+
+        self.pieces = []
+        self._load_pieces()
+        self.scene = in_scene
+
+    def _load_pieces(self):
+        """Builds the overall set:
+        - Loads images from the sprite sheet.
+        - Creates a Piece object, and sets appropriate attributes
+          for that piece.
+        - Adds each piece to the list self.pieces.
+        """
+        filename = 'Galaga-GeneralSprites.png'
+        piece_ss = SpriteSheet(filename)
+
+        # Create a black king.
+        player_rect = (0, 0, 30, 30)
+        player_image = piece_ss.image_at(player_rect)
+
+        player_ship = PlayerShip(self.scene)
+        player_ship.image = player_image
+        self.pieces.append(player_ship)
+
+
+class PlayerShip(GameObject):
+    def __init__(self, in_scene: "Scene"):
+        GameObject.__init__(self, 50, 50, in_scene)
+        self.add(self.scene.groups.get("player"))
+        self.add(self.scene.groups.get("all_sprites"))
+        self.image = None
+        self.screen = engine.Engine.screen
+        self.x = 480
+        self.y = 100
+
+    def blitme(self):
+        self.rect = self.image.get_rect()
+        self.rect.topleft = self.x, self.y
+        self.screen.blit(self.image, self.rect)
+
+
+
+
+
+
